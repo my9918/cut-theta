ec4b7af1692772667762b7e1fe0df8020a32a349
my9918 <s20c1125rk@s.chibakoudai.jp>
2022-10-02 22:13:45 +0900

    path fix & chenge txt name

--- cut-theta.py
+++ cut-theta.py
@@ -1,16 +1,20 @@
 import cv2
 import numpy as np
 import math
-img = cv2.imread("IMG_4122.JPG")
-#xyxy to yyxx 
-img1 = img[649:1725,1281:4032]
 
+img_origin = cv2.imread("IMG_4122.JPG")
+#xyxy to yyxx 
+img = img_origin[649:1725,1281:4032]
+
+#白黒のネガにする
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("1nega.png", gray)
+#cv2.imwrite("1nega.png", gray)
+
 #reversed_gray = cv2.bitwise_not(gray) これよりも下のCannyのほうが良さげ
 #cv2.imwrite("gray.png", reversed_gray)
+
 edges = cv2.Canny(gray,90,450,apertureSize = 3)
-cv2.imwrite("2canny.png", edges)
+#cv2.imwrite("2canny.png", edges)
 
 right=10
 left=-10
@@ -23,10 +27,10 @@ for line in lines:
     x1, y1, x2, y2 = line[0]
     rad = math.atan2(y2 - y1, x2 - x1)
     deg = np.rad2deg(rad)
-    with open('test.txt', 'a') as f:
+    with open('deg.txt', 'a') as f:
         content = str(deg)
         f.write(content + '\n')
-    with open('test2.txt', 'a') as f:
+    with open('line.txt', 'a') as f:
         content2 = str(line)
         f.write(content2 + '\n')
     if (a < rad) or (rad < b ):
